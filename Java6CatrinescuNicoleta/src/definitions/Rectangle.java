f51c616fa8e220cb490413c36dfeb594fdb39ede
Amastasia <Amastasia@Amastasia-PC>
2018-01-14 16:21:54 +0200

    Homework group 6

---
+++ Java6CatrinescuNicoleta/src/definitions/Rectangle.java
@@ -0,0 +1,25 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package definitions;
+
+/**
+ *
+ * @author Amastasia
+ */
+public class Rectangle extends Figure {
+
+    /**
+     *
+     * @return
+     */
+    @Override
+    public String getDefinitions(){
+            return "";
+    
+}
+}
+
+    
